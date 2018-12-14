ec1a5e04af4de3c9e591641cfd4ad5ab17664c63
dynnoil <dynnoil@ya.ru>
2018-12-14 14:18:24 +0300

    jQuery usages

--- dialog/dialog-module.js
+++ dialog/dialog-module.js
@@ -1,6 +1,8 @@
 (function () {
     'use strict';
 
+    if ($ == undefined || jQuery == undefined) throw new Error('jQuery library is not found');
+
     function* generateId(prefix = 'id', initialNumber = 1, maxAmount = 100) {
         let index = initialNumber;
         while (index < maxAmount) {
@@ -26,33 +28,30 @@
         this.x = x;
         this.y = y;
         this.backgroundColor = backgroundColor;
-        this._parentElement = null;
         this.onOkButtonClick = undefined;
         this.onCancelButtonClick = undefined;
     }
 
     Dialog.prototype._prepareHandlers = function () {
-        const dialog = document.getElementById(this._id);
-        const okButton = dialog.getElementsByClassName(OK_BUTTON_CLASS)[0];
-        const okButtonHandler = this.onOkButtonClick ? this.onOkButtonClick.bind(this) : this.hide.bind(this);
-        okButton.addEventListener('click', okButtonHandler, false);
-        const cancelButton = dialog.getElementsByClassName(CANCEL_BUTTON_CLASS)[0];
-        const cancelButtonHandler = this.onCancelButtonClick ? this.onCancelButtonClick.bind(this) : this.hide.bind(this);
-        cancelButton.addEventListener('click', cancelButtonHandler, false);
+        const okButton = $(`#${this._id} .${OK_BUTTON_CLASS}`);
+        okButton.on('click', this.onOkButtonClick && this.onOkButtonClick.bind(this));
+        const cancelButton = $(`#${this._id} .${CANCEL_BUTTON_CLASS}`);
+        cancelButton.on('click', this.onCancelButtonClick && this.onCancelButtonClick.bind(this));
     }
 
     Dialog.prototype.show = function (tagId = '') {
-        const elementByTag = document.getElementById(tagId);
-        this._parentElement = elementByTag ? elementByTag : document.getElementsByTagName('body')[0];
-        this._parentElement.innerHTML += this;
+        const elementByTag = $(`#${tagId}`);
+        const parentElement = elementByTag ? elementByTag : $('body');
+        parentElement.append('' + this);
         this._prepareHandlers();
     }
 
     Dialog.prototype.hide = function () {
-        const dialog = document.getElementById(this._id);
-        if (this._parentElement != null) {
-            this._parentElement.removeChild(dialog);
-        }
+        $(`#${this._id}`).css('display', 'none');
+    }
+
+    Dialog.prototype.remove = function () {
+        $(`#${this._id}`).remove();
     }
 
     Dialog.prototype.toString = function () {
